bdfdaeccbc58ca4fe1dcd702bd5645ae0efe68ed
stew anoya <stewartanoya@gmail.com>
2021-11-02 13:16:07 +0000

    created a function using settimeout to animate sentence

---
+++ typewriter.js
@@ -0,0 +1,16 @@
+const sentence = "hello there from lighthouse labs";
+
+let typewriter = () => {
+  let timer = 0;
+  for (const char of sentence) {
+    setTimeout(() => {
+      process.stdout.write(`${char}`);
+    }, timer);
+    timer += 50;
+  }
+  setTimeout(() => {
+    console.log("\n");
+  }, 2000);
+};
+
+typewriter(sentence);
